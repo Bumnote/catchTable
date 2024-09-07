f70ff8170fa812dec0a34f1c58d3806cb6c89ad0
YongBum <dt10002@naver.com>
2024-09-08 02:14:43 +0900

    chore: 기초작업

---
+++ src/main/java/catchtable/cooking/controller/BasicController.java
@@ -0,0 +1,14 @@
+package catchtable.cooking.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BasicController {
+
+    @GetMapping("/hello")
+    public String index() {
+        return "hello";
+    }
+
+}
