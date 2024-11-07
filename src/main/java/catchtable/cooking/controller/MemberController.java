b07543db9dfa1a7989c7bb29beddc2141376eceb
YongBum <dt10002@naver.com>
2024-11-07 23:23:59 +0900

    [Feat] 로그인 기능 구현

--- src/main/java/catchtable/cooking/controller/MemberController.java
+++ src/main/java/catchtable/cooking/controller/MemberController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -21,5 +22,18 @@ public class MemberController {
         memberService.register(new MemberCreateParam().of(memberRequest));
         return CommonResponse.of(Code.OK);
     }
+
+    @PostMapping("/api/member/login")
+    public CommonResponse<?> login(@RequestBody LoginRequest loginRequest) {
+        JwtToken jwtToken = memberService.authenticate(new LoginCreateParam().of(loginRequest));
+        return CommonResponse.of(jwtToken);
+    }
+
+    @PostMapping("/api/test")
+    public CommonResponse<?> test(@RequestHeader("Authorization") String token) {
+        log.info("token: {}", token);
+        return CommonResponse.of(token);
+    }
+
 
 }
---
+++ src/main/java/catchtable/cooking/jwt/JwtTokenInterceptor.java
@@ -0,0 +1,60 @@
+package catchtable.cooking.jwt;
+
+import catchtable.cooking.exception.Code;
+import catchtable.cooking.exception.CustomException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtTokenInterceptor implements HandlerInterceptor {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private static final String TOKEN_HEADER = "Authorization";
+    private static final String TOKEN_PREFIX = "Bearer ";
+
+    public boolean preHandle(@NonNull HttpServletRequest request,
+                             @NonNull HttpServletResponse response,
+                             @NonNull Object handler) throws IOException {
+
+        String token = resolveToken(request);
+
+        log.info("access token: {}", token);
+        log.info("request: {}", request);
+
+        log.info("request path: {}", request.getServletPath());
+        log.info("request header: {}", request.getHeader("Authorization"));
+        log.info("request method: {}", request.getMethod());
+        log.info("request contentType: {}", request.getContentType());
+        log.info("request toString: {}", request.toString());
+
+        if (StringUtils.hasText(token) && jwtTokenProvider.validateToken(token)) {
+            return true;
+        }
+        response.setStatus(401);
+        throw new CustomException(Code.ACCESS_TOKEN_UNAUTHORIZED);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        log.info("resolve 토큰 검증 request: {}", request);
+        String bearerToken = request.getHeader(TOKEN_HEADER);
+
+        log.info("resolve bearerToken: {}", bearerToken);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(TOKEN_PREFIX)) {
+            log.info("resolve 토큰 검증 완료!");
+            return bearerToken.substring(TOKEN_PREFIX.length());
+        }
+        return null;
+    }
+
+}
