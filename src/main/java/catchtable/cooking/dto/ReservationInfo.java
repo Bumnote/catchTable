468f3fe3bcec455cd03f000f723171c4434599a7
YongBum <dt10002@naver.com>
2025-01-05 05:34:36 +0900

    [Refactor] 기초적인 코드 수정
    [수정 내용]
    - DTO 초기화 시, 초기화 어노테이션 @Builder.Default 추가
    - String 타입으로 받던 날짜를 LocalDate 타입으로 변경

--- src/main/java/catchtable/cooking/dto/ReservationInfo.java
+++ src/main/java/catchtable/cooking/dto/ReservationInfo.java
@@ -3,12 +3,14 @@ package catchtable.cooking.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Getter
 @NoArgsConstructor
 public class ReservationInfo {
 
     private Integer personCount;
 
-    private String date;
+    private LocalDate date;
 
 }
